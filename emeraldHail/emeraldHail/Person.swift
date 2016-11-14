cf7dd3b0c332a4facbbc090bca0b25a667d33280
Henry Ly <henryly@gmail.com>
2016-11-14 17:52:00 -0500

    Add BloodType and Gender

---
+++ emeraldHail/emeraldHail/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  emeraldHail
+//
+//  Created by Henry Ly on 11/14/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum BloodType: String {
+    case OPos, ONeg, APos, ANeg, BPos, BNeg, ABPost, ABNeg
+}
+
+enum Gender: String {
+    case female, male
+}
