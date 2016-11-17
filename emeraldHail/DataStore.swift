169dd413ba30fbed0781e61970a4b13240ae9581
EnriqueTor <etorrendell@gmail.com>
2016-11-17 14:39:26 -0500

    Create DataStore file

---
+++ emeraldHail/DataStore.swift
@@ -0,0 +1,13 @@
+//
+//  DataStore.swift
+//  emeraldHail
+//
+//  Created by Enrique Torrendell on 11/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+
