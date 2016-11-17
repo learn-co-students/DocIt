e581256aa381d3c66900b16004d7d8c38aa6fd16
EnriqueTor <etorrendell@gmail.com>
2016-11-17 14:48:23 -0500

    update DataStore file

--- emeraldHail/DataStore.swift
+++ emeraldHail/DataStore.swift
@@ -9,5 +9,34 @@
 import Foundation
 import CoreData
 
+class DataStore{
+    private init(){}
+    static let sharedInstance = DataStore()
+    
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Todo")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
 
 
