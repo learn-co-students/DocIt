0a895baa8b71f4e598c99790b2a5b0c6a001cf13
Luna An <mimicatcodes@gmail.com>
2016-11-17 14:50:11 -0500

    Updated gitignore

---
+++ emeraldHail/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  emeraldHail
+//
+//  Created by Mirim An on 11/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+
+// Secret info goes here. 
