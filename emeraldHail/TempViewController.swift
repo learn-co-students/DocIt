30c0d8828c94898537bf84ce6bc4f062203f8393
Taniraw <tanirawrites@gmail.com>
2016-11-17 15:39:40 -0500

    tempview added

---
+++ emeraldHail/TempViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TempViewController.swift
+//  emeraldHail
+//
+//  Created by Tanira Wiggins on 11/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
