d6e1c9074f887df7e79c04753f76d04b18fb4838
Henry Ly <henryly@gmail.com>
2016-11-18 09:57:53 -0500

    Add Family and Member.swift

---
+++ emeraldHail/Family.swift
@@ -0,0 +1,20 @@
+//
+//  Family.swift
+//  emeraldHail
+//
+//  Created by Henry Ly on 11/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Family {
+    
+    var name: String
+    var email: String
+    var coverImage: UIImage?
+    var id: String
+    var members: [Member]
+    
+}
---
+++ emeraldHail/Member.swift
@@ -0,0 +1,25 @@
+//
+//  Member.swift
+//  emeraldHail
+//
+//  Created by Henry Ly on 11/18/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+struct Member {
+    
+    var firstName: String
+    var lastNAme: String
+    var id: String
+    var isFemale: Bool
+    var profileImage: UIImage?
+    var bloodType: BloodType
+    var height: Double
+    var weight: Double
+    var events: [Event]
+    
+}
+
+
