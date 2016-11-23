8941bb1e94bbd98774a76057b7830f056a1d28ef
Taniraw <tanirawrites@gmail.com>
2016-11-23 14:59:21 -0500

    note-vc-added-code-works-textField-doesnt

--- emeraldHail/AddNotesViewController.swift
+++ emeraldHail/AddNotesViewController.swift
@@ -38,19 +38,23 @@ class AddNotesViewController: UIViewController {
     }
     
     @IBAction func addNotesSaveButton(_ sender: Any) {
-        
-            guard let note = addNotesTextField.text, note != "" else { return }
-            let databasePostRef = database.child("Post").child((FIRAuth.auth()?.currentUser?.uid)!).childByAutoId()
-            let uniqueID = databasePostRef.key
-            let post = Post(note: note)
-            databasePostRef.setValue(post.serialize(), withCompletionBlock: {error, FIRDatabaseReference in
-                 self.dismiss(animated: true, completion: nil)
+        guard let note = addNotesTextField.text, note != "" else { return }
+        let databasePostRef = database.child("Post").child((FIRAuth.auth()?.currentUser?.uid)!).childByAutoId()
+        let uniqueID = databasePostRef.key
+        let post = Post(note: note)
+        databasePostRef.setValue(post.serialize(), withCompletionBlock: {error, FIRDatabaseReference in
+        self.dismiss(animated: true, completion: nil)
                 
             })
             
-            
-        }
         
+        
+        }
+    
+    func displayKeyboard(){
+        self.addNotesTextField.becomeFirstResponder()
+        self.dismiss(animated: true, completion: nil)
+    }
         
         
     }
