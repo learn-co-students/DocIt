9fc12d12372021113822fdeb6722d49e12e4551d
EnriqueTor <etorrendell@gmail.com>
2016-11-24 01:25:11 -0500

    add code to symptomVC

--- emeraldHail/SymptomsViewController.swift
+++ emeraldHail/SymptomsViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class SymptomsViewController: UIViewController {
+class SymptomsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // OUTLETS 
+    
+    @IBOutlet weak var symptomTableView: UITableView!
+    
+    // PROPERTIES
+    
+    var symptoms:[Symptom] = [.bloodInStool, .chestPain, .constipation, .cough, .diarrhea, .dizziness, .earache, .eyeDiscomfort, .fever, .footPain, .footSwelling, .headache, .heartpalpitations, .itchiness, .kneePain, .legSwelling, .musclePain, .nasalcongestion, .nausea, .neckPain, .shortBreath, .shoulderPain, .skinRashes, .soreThroat, .urinaryProblems, .vision, .vomiting, .wheezing]
+    
+    // LOADS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        symptomTableView.reloadData()
     }
+    
+    // TABLEVIEW METHODS
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return symptoms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "symptomCell", for: indexPath) as! SymptomViewCell
+        
+        cell.symptomLabel.text = symptoms[indexPath.row].rawValue
+        
+        return cell
+    }
+    
+}
+
+class SymptomViewCell: UITableViewCell {
+
+    // OUTLETS 
+    
+    @IBOutlet weak var symptomLabel: UILabel!
     
 
 }
