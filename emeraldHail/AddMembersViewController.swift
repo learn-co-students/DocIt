4544201e0295c97f355980339b3be73e060fcbf0
Luna An <mimicatcodes@gmail.com>
2016-11-29 15:01:19 -0500

    Default gender created

--- emeraldHail/AddMembersViewController.swift
+++ emeraldHail/AddMembersViewController.swift
@@ -12,15 +12,15 @@ import SDWebImage
 
 class AddMembersViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
+    var selectedGender = "Female"
+    
     // OUTLETS
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var birthdayField: UITextField!
-    @IBOutlet weak var genderField: UITextField!
-
-
+   
     // LOADS
     
     override func viewDidLoad() {
@@ -36,8 +36,9 @@ class AddMembersViewController: UIViewController, UIImagePickerControllerDelegat
             let lastName = lastNameField.text, lastName != "",
             let dob = birthdayField.text, dob != ""
             else { return }
-        let gender = Logics.sharedInstance.genderType
         
+        let gender = selectedGender
+        print("GENDERRRRRRRR is \(gender)")
         let database: FIRDatabaseReference = FIRDatabase.database().reference()
         let databaseMembersRef = database.child("members").child((FIRAuth.auth()?.currentUser?.uid)!).childByAutoId()
         let uniqueID = databaseMembersRef.key
@@ -71,9 +72,11 @@ class AddMembersViewController: UIViewController, UIImagePickerControllerDelegat
         
         switch sender.selectedSegmentIndex {
         case 0:
-            Logics.sharedInstance.genderType = Gender.female.rawValue
+            selectedGender = Gender.female.rawValue
+            Logics.sharedInstance.genderType = selectedGender
         case 1:
-            Logics.sharedInstance.genderType = Gender.male.rawValue
+            selectedGender = Gender.male.rawValue
+            Logics.sharedInstance.genderType = selectedGender
         default:
             break
         }
