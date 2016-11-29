664e41426c222bca952ce89a9920643ee33e259c
Taniraw <tanirawrites@gmail.com>
2016-11-29 17:22:09 -0500

    editedDatePicker-forAccurateDOB

--- emeraldHail/EditMemberSettingsViewController.swift
+++ emeraldHail/EditMemberSettingsViewController.swift
@@ -11,7 +11,7 @@ import FirebaseDatabase
 import FirebaseStorage
 import Firebase
 
-class EditMemberSettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class EditMemberSettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var firstNameTextField: UITextField!
     
@@ -34,6 +34,8 @@ class EditMemberSettingsViewController: UIViewController, UIPickerViewDataSource
     var database : FIRDatabaseReference = FIRDatabase.database().reference()
     var postRef: FIRDatabaseReference = FIRDatabase.database().reference().child("members")
     let storage: FIRStorage = FIRStorage.storage()
+   
+    
     // Member Profile Properties
     var firstName: String?
     var lastName: String?
@@ -42,18 +44,29 @@ class EditMemberSettingsViewController: UIViewController, UIPickerViewDataSource
     var bloodTypeSelected: String?
     
     
-    // Pickers
+
     
     var bloodSelection = UIPickerView()
-    var dobSelection = UIDatePicker()
+    
     
     //timezone datepicker
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // TextField Delegates
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        dobTextField.delegate = self
+        bloodType.delegate = self
+        
+        
+        
+        // Blood Type Picker View
         bloodSelection.delegate = self
         bloodSelection.dataSource = self
         bloodType.inputView = bloodSelection
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -63,22 +76,58 @@ class EditMemberSettingsViewController: UIViewController, UIPickerViewDataSource
         
     }
  
+    //MARK: DatePicker -> DOB Text Field
     
     
+    // return NO to disallow editing.
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool{
+        
+        return true
+    }
     
-    // returns the number of 'columns' to display.
-  
+    // became first responder
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        let dobSelection = UIDatePicker()
+        dobTextField.inputView = dobSelection
+        dobSelection.datePickerMode = UIDatePickerMode.date
+        dobSelection.addTarget(self, action: #selector(self.datePickerChanged(sender:)) , for: .valueChanged)
+    }
+
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dobTextField.resignFirstResponder()
+        return true
+    }
+    
+    // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    
+    func datePickerChanged(sender: UIDatePicker) {
+         let myLocale = Locale(identifier: "en_US")
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.dateFormat = "MM-dd-yyyy"
+        var calendar = Calendar(identifier: .gregorian)
+        dobTextField.text = formatter.string(from: sender.date)
+        
+        
+    }
+    
+    // MARK: PickerView methods for Blood Type
+    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int{
         return 1
     }
     
-    
-    // returns the # of rows in each component..
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         return bloodTypeSelections.count
     }
     
-    // Saves the pickerView selection for bloodType.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         bloodType.text = bloodTypeSelections[row]
     }
