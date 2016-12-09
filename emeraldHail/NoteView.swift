c7cd41a5e948bd6cfe9c5ef91023c2353ab9ceb1
Henry Ly <henryly@gmail.com>
2016-12-09 17:45:24 -0500

    New icons and logo

--- emeraldHail/NoteView.swift
+++ emeraldHail/NoteView.swift
@@ -51,7 +51,7 @@ class NoteView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        drawTimeline(circleColor: Constants.Colors.scooter.cgColor, lineColor: Constants.Colors.submarine.cgColor)
+        drawTimeline(circleColor: Constants.Colors.corn.cgColor, lineColor: Constants.Colors.submarine.cgColor)
     }
     
     
