c6cfe2cbb45f18bddd114dec875e8bd54d87eb71
Henry Ly <henryly@gmail.com>
2016-12-08 22:43:00 -0500

    inviteFamilyID

--- emeraldHail/LoginViewController.swift
+++ emeraldHail/LoginViewController.swift
@@ -13,41 +13,41 @@ import GoogleSignIn
 
 
 class LoginViewController: UIViewController {
-
+    
     // MARK: - Outlets
-
+    
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var signIn: UIButton!
-
+    
     // MARK: - Properties
-
+    
     let store = DataStore.sharedInstance
     var database: FIRDatabaseReference = FIRDatabase.database().reference()
     
     // MARK: - Loads
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupViews()
         hideKeyboardWhenTappedAround()
-
+        
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         setupViews()
-
+        
     }
-
+    
     // MARK: - Actions
-
+    
     @IBAction func signIn(_ sender: UIButton) {
         login()
         signIn.isEnabled = false
     }
-
+    
     @IBAction func forgotPressed(_ sender: Any) {
         let alert = UIAlertController(title: "Forgot?", message: "Please enter your login email address.\n\nWe'll send you an email with instructions on how to reset your password.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
@@ -66,13 +66,13 @@ class LoginViewController: UIViewController {
         alert.addAction(cancelAction)
         present(alert, animated: true, completion: nil)
     }
-
+    
     @IBAction func createAccountPressed(_ sender: Any) {
         
         NotificationCenter.default.post(name: .openRegisterVC, object: nil)
-      //  self.performSegue(withIdentifier: "showCreateAccount", sender: nil)
+        //  self.performSegue(withIdentifier: "showCreateAccount", sender: nil)
     }
-
+    
     // This function enables/disables the signIn button when the fields are empty/not empty.
     @IBAction func textDidChange(_ sender: UITextField) {
         if !(emailField.text?.characters.isEmpty)! && !(passwordField.text?.characters.isEmpty)! {
@@ -83,52 +83,52 @@ class LoginViewController: UIViewController {
             signIn.backgroundColor = UIColor.lightGray
         }
     }
-
-
+    
+    
     @IBAction func pressedGoogleSignIn(_ sender: Any) {
-            GIDSignIn.sharedInstance().signIn()
+        GIDSignIn.sharedInstance().signIn()
         print("We are hitting the google button")
-
+        
     }
-
+    
     // MARK: - Methods
-
+    
     func setupViews() {
         // Make the email field become the first repsonder and show keyboard when this vc loads
         emailField.becomeFirstResponder()
-
+        
         // Set error label to "" on viewDidLoad
         // Clear the text fields when logging out and returning to the login screen
         errorLabel.text = nil
         emailField.text = nil
         passwordField.text = nil
-
+        
         emailField.layer.cornerRadius = 2
         emailField.layer.borderColor = UIColor.lightGray.cgColor
-
+        
         passwordField.layer.cornerRadius = 2
         passwordField.layer.borderColor = UIColor.lightGray.cgColor
-
+        
         signIn.isEnabled = false
         signIn.backgroundColor = UIColor.lightGray
         signIn.layer.cornerRadius = 2
     }
-
+    
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboardView))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-
+    
     func dismissKeyboardView() {
         view.endEditing(true)
     }
-
-
+    
+    
     func login() {
         
         guard let email = emailField.text, let password = passwordField.text else { return }
-
+        
         FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
             if let error = error {
                 // TODO: Format the error.localizedDescription for natural language, ex. "Invalid email", "Password must be 6 characters or more", etc.
@@ -139,44 +139,52 @@ class LoginViewController: UIViewController {
             }
             // Set the sharedInstance familyID to the current user.uid
             
-            self.database.child("user").child((user?.uid)!).observe(.value, with: { snapshot in
+            if self.store.inviteFamilyID == "" {
                 
-                DispatchQueue.main.async {
-                    
-                var data = snapshot.value as? [String:Any]
                 
-                    guard let familyID = data?["familyID"] as? String else { return }
+                self.database.child("user").child((user?.uid)!).observe(.value, with: { snapshot in
                 
-                print("======> \(familyID)")
+                    DispatchQueue.main.async {
+                        
+                        var data = snapshot.value as? [String:Any]
+                        
+                        guard let familyID = data?["familyID"] as? String else { return }
+                        
+                        print("======> \(familyID)")
+                        
+                        self.store.user.id = (user?.uid)!
+                        self.store.user.familyId = familyID
+                        self.store.family.id = familyID
+                    }
+                })
+            } else {
                 
                 self.store.user.id = (user?.uid)!
-                self.store.user.familyId = familyID
-                self.store.family.id = familyID
+                self.store.user.familyId = self.store.inviteFamilyID
+                self.database.child("user").child(self.store.user.id).child("familyID").setValue(self.store.user.familyId)
                 
-                    
-               
-                }
-                
-                
-                
-                
-            })
-//            NotificationCenter.default.post(name: .openfamilyVC, object: nil)
+            }
             
             
             
-            
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
-                
-                NotificationCenter.default.post(name: .openfamilyVC, object: nil)
-            
-            })
-//            self.performSegue(withIdentifier: "showFamily", sender: nil)
-            
-
         }
+        
+        //            NotificationCenter.default.post(name: .openfamilyVC, object: nil)
+        
+        
+        
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            
+            NotificationCenter.default.post(name: .openfamilyVC, object: nil)
+            
+        })
+        //            self.performSegue(withIdentifier: "showFamily", sender: nil)
+        
+        
     }
-
-
 }
+
+
+
