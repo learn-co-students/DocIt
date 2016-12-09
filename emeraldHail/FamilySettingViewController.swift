4d7d12c5d5db63f54ab4d19c23912a585acda88a
Henry Ly <henryly@gmail.com>
2016-12-08 22:28:45 -0500

    Show share sheet

--- emeraldHail/FamilySettingViewController.swift
+++ emeraldHail/FamilySettingViewController.swift
@@ -17,26 +17,26 @@ import MessageUI
 import Branch
 
 class FamilySettingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
-
+    
     // MARK: - Outlets
     
     @IBOutlet weak var touchID: UISegmentedControl!
     @IBOutlet weak var joinFamily: JoinFamily!
     
     // MARK: - Properties
-
+    
     let store = DataStore.sharedInstance
-
+    
     // MARK: - Loads
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         checkTouchID()
     }
-
+    
     // MARK: - Actions
-
+    
     @IBAction func changeFamilyPic(_ sender: UIButton) {
         handleSelectProfileImageView()
     }
@@ -44,7 +44,7 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
     @IBAction func changeFamilyNamePressed(_ sender: Any) {
         changeFamilyName()
     }
-
+    
     @IBAction func logoutPressed(_ sender: Any) {
         do {
             try FIRAuth.auth()?.signOut()
@@ -55,16 +55,16 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
             print ("Error signing out: \(signOutError.localizedDescription)")
         }
     }
-
+    
     @IBAction func touchIDOnOff(_ sender: UISegmentedControl) {
         
         if touchID.selectedSegmentIndex == 0 {
-        
+            
             touchID(activate: false)
             deleteAllData(entity: "CurrentUser")
             
         }
-        
+            
         else if touchID.selectedSegmentIndex == 1 {
             
             touchID(activate: true)
@@ -86,18 +86,11 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
             
         }
     }
-
+    
     @IBAction func didPressInviteParent(_ sender: UIButton) {
         print("didPressInviteParent pressed")
         
         let branchUniversalObject: BranchUniversalObject = BranchUniversalObject(canonicalIdentifier: "123123123")
-        
-        
-        let linkProperties: BranchLinkProperties = BranchLinkProperties()
-        linkProperties.feature = "invite"
-        linkProperties.channel = "SMS"
-        linkProperties.addControlParam("$ios_url", withValue: "docit://")
-        linkProperties.addControlParam("familyID", withValue: "HELLOHENRY")
         
         let newLinkProperties: BranchLinkProperties = BranchLinkProperties()
         newLinkProperties.feature = "invite"
@@ -105,33 +98,30 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
         newLinkProperties.addControlParam("$ios_url", withValue: "docit://")
         newLinkProperties.addControlParam("inviteFamilyID", withValue: store.user.familyId)
         
-        branchUniversalObject.getShortUrl(with: linkProperties) { (url, error) in
-            if error == nil {
-                print("1. got my Branch link to share: \(url)")
-            }
-        }
+        //        branchUniversalObject.getShortUrl(with: newLinkProperties) { (url, error) in
+        //            if error == nil {
+        //                print("2. got my Branch link to share: \(url)")
+        //            }
+        //        }
         
-        branchUniversalObject.getShortUrl(with: newLinkProperties) { (url, error) in
-            if error == nil {
-                print("2. got my Branch link to share: \(url)")
-            }
+        branchUniversalObject.showShareSheet(with: newLinkProperties, andShareText: "Please join my family on Doc It!", from: self) { (activityType, completed) in
+            print("done showing share sheet!")
         }
         
         print(branchUniversalObject)
         
-        
     }
     
     // MARK: - Methods
-
+    
     func changeFamilyName() {
         let alert = UIAlertController(title: nil, message: "Change your family name", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
             let userInput = alert.textFields![0].text
             let ref = FIRDatabase.database().reference().child("family").child(self.store.family.id)
-
+            
             guard let name = userInput, name != "" else { return }
-
+            
             ref.updateChildValues(["name": name], withCompletionBlock: { (error, dataRef) in
                 if let error = error {
                     print(error.localizedDescription)
@@ -145,29 +135,29 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
         alert.addAction(cancelAction)
         present(alert, animated: true, completion: nil)
     }
-
+    
     func changeFamilyCoverPic(photo: UIImage, handler: @escaping (Bool) -> Void) {
-
+        
         let database = FIRDatabase.database().reference()
         let familyDatabase = database.child("family").child(store.family.id)
         let storageRef = FIRStorage.storage().reference(forURL: "gs://emerald-860cb.appspot.com")
         let storeImageRef = storageRef.child("familyImages").child(store.family.id)
-
+        
         if let uploadData = UIImageJPEGRepresentation(photo, 0.25) {
-
+            
             storeImageRef.put(uploadData, metadata: nil, completion: { (metadata, error) in
                 if error != nil {
                     print(error?.localizedDescription ?? "Error in changeFamilyCoverPic")
-
+                    
                     return
                 }
-
+                
                 if let familyPicString = metadata?.downloadURL()?.absoluteString {
-
+                    
                     familyDatabase.updateChildValues(["coverImageStr": familyPicString], withCompletionBlock: { (error, dataRef) in
-
+                        
                         DispatchQueue.main.async {
-
+                            
                             handler(true)
                         }
                     })
@@ -175,39 +165,39 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
             })
         }
     }
-
+    
     func handleSelectProfileImageView(){
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
         picker.allowsEditing = true
-
+        
         self.present(picker, animated: true, completion: nil)
-
+        
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-
+        
         var selectedImageFromPicker: UIImage?
-
+        
         if let editImage = info[UIImagePickerControllerEditedImage] as? UIImage {
-
+            
             selectedImageFromPicker = editImage
-
+            
         } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             selectedImageFromPicker = originalImage
         }
-
+        
         if let selectedImage = selectedImageFromPicker {
-
+            
             changeFamilyCoverPic(photo: selectedImage, handler: { success in
-
+                
                 self.dismiss(animated: true, completion: nil)
-
+                
             })
         }
     }
-
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         print("picked canceled")
         dismiss(animated: true, completion: nil)
@@ -232,7 +222,7 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true)
     }
-
+    
     // Mark: Methods Touch ID
     
     func checkTouchID() {
@@ -241,14 +231,14 @@ class FamilySettingViewController: UIViewController, UIImagePickerControllerDele
         
         database.observe(.value, with: { (snapshot) in
             
-          let value = snapshot.value as? Bool
+            let value = snapshot.value as? Bool
             
             if value == true {
                 
                 self.touchID.selectedSegmentIndex = 1
                 
             }
-            
+                
             else if value == false {
                 
                 self.touchID.selectedSegmentIndex = 0
