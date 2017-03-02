bee9aebfa5a6cdfc989169dc79138614a73993f2
Henry M Ly <henrymly@gmail.com>
2017-03-02 12:24:52 -0800

    clean forgot pw

--- emeraldHail/ForgotPasswordViewController.swift
+++ emeraldHail/ForgotPasswordViewController.swift
@@ -11,106 +11,69 @@ import Firebase
 
 class ForgotPasswordViewController: UIViewController {
 
-    // MARK: - Outlets
-    
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var forgotView: UIView!
  
-    
-    // MARK: - Properties
-    
     let store = DataStore.sharedInstance
-    
-    // MARK: - Loads
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
-        
     }
     
-    
-    // MARK: - Actions
-    
-
     @IBAction func cancel(_ sender: UIButton) {
-        
         dismiss(animated: true, completion: nil)
     }
     
     
     @IBAction func save(_ sender: Any) {
-        
             sendEmail()
     }
     
-    
     @IBAction func tapDismiss(_ sender: Any) {
-        
         dismiss(animated: true, completion: nil)
-    
     }
     
     @IBAction func editingFamilyName(_ sender: UITextField) {
-        
         if emailTextField.text?.isEmpty == true {
-            
             sendButton.isEnabled = false
             sendButton.backgroundColor = Constants.Colors.submarine
-            
         } else {
-            
             sendButton.isEnabled = true
             sendButton.backgroundColor = Constants.Colors.scooter
-            
         }
-        
     }
 
-   
     // MARK: - Methods
-    
     func setupViews() {
-        
         view.backgroundColor = Constants.Colors.transBlack
         forgotView.docItStyleView()
+        cancelButton.docItStyle()
+        emailTextField.docItStyle()
+        sendButton.docItStyle()
+        
         
         sendButton.isEnabled = false
         sendButton.backgroundColor = Constants.Colors.submarine
-        sendButton.docItStyle()
-        
-        cancelButton.docItStyle()
-        
-        emailTextField.docItStyle()
-        
+
         emailTextField.becomeFirstResponder()
-        
-        
-        
     }
     
-    
     func sendEmail() {
-        
         sendButton.isEnabled = false
         
         guard let userInput = emailTextField.text else { return }
-        
+    
         FIRAuth.auth()?.sendPasswordReset(withEmail: userInput, completion: { (error) in
             if let error = error {
+                // TODO: Handle errors
                 print(error.localizedDescription)
+                self.sendButton.isEnabled = true
             }
         })
-
-        
-        
         dismiss(animated: true, completion: nil)
-        
     }
     
-
-
 }
